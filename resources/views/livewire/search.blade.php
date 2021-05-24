43c50713b843ce7ad04ce288509896847a42b2a4
Luifer1994 <69922768+Luifer1994@users.noreply.github.com>
2021-05-24 01:59:59 -0500

    header

---
+++ resources/views/livewire/search.blade.php
@@ -0,0 +1,6 @@
+<div class="flex-1 relative">
+    <x-jet-input type="text" class="w-full" placeholder="¿Estás buscando algún producto?"/>
+    <button class="absolute bg-orange-500 top-0 right-0 w-12 h-full flex items-center justify-center rounded-r-md">
+        <x-search size="35" color="white"/>
+    </button> 
+</div>
